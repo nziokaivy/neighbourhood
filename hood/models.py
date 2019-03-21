921f9e0607aa3bbf8942c3c037708b5030211c8b
nziokaivy <nziokaivy@gmail.com>
2019-03-21 18:26:21 +0300

    add models

--- hood/models.py
+++ hood/models.py
@@ -3,24 +3,11 @@ from django.contrib.auth.models import User
 
 
 class Neighbourhood(models.Model):
-    CITY_CHOICES = (
-        ('Nairobi', 'Nairobi'),
-        ('Nakuru', 'Nakuru'),
-        ('Mombasa', 'Mombasa'),
-        ('Machakos', 'Machakos'),
-        ('Kisumu', 'Kisumu'),
-        ('Eldoret', 'Eldoret'),
-        ('Malindi', 'Malindi'),
-        ('Diani', 'Diani'),
-        ('Naivasha', 'Naivasha'),
-        ('Kiambu', 'Kiambu'),
-
-    )
-
     neighbourhood_name = models.CharField(max_length=30)
     neighbourhood_location = models.CharField(choices=CITY_CHOICES, max_length=200 ,default=0, null=True, blank=True)
     neighbourhood_count= models.IntegerField(default=0, null=True, blank=True)
-    
+    user_profile = models.ForeignKey(User, on_delete=models.CASCADE, related_name='hoods', blank=True)
+
     def __str__(self):
         return self.neighbourhood_name
 
@@ -60,6 +47,8 @@ class Business(models.Model):
     neighbourhood_id = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE,related_name="neighbourhoodbusiness",null=True,blank=True)
     business_email_address = models.CharField(max_length=200, null = True)
     pub_date = models.DateTimeField(auto_now_add=True)
+
+  
     def __str__(self):
         return self.business_name
 
@@ -130,3 +119,82 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
+
+class Business(models.Model):
+    name = models.CharField(max_length=30)
+    description = HTMLField(blank=True)
+    email = models.EmailField(max_length=70, blank=True)
+    biz_owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    biz_hood = models.ForeignKey(
+        Neighborhood, on_delete=models.CASCADE, related_name='biz', null=True)
+
+    '''
+    this is added to ensure the linter has no errors saying class has no objects member in VS Code IDE
+    '''
+    objects = models.Manager()
+
+    @classmethod
+    def search_by_name(cls, search_term):
+        businesses = cls.objects.filter(name__icontains=search_term)
+        return businesses
+
+    @classmethod
+    def get_neighborhood_businesses(cls, neighborhood_id):
+        businesses = Business.objects.filter(neighborhood_id=id)
+        return businesses
+
+    @classmethod
+    def get_hood_biz(cls, biz_hood):
+        businesses = Business.objects.filter(biz_hood_pk=biz_hood)
+        return businesses
+
+    @classmethod
+    def get_profile_businesses(cls, profile):
+        businesses = Business.objects.filter(biz_owner__pk=profile)
+        return businesses
+
+
+class Join(models.Model):
+    '''
+Updating user location each time they join or leave a neghborhood	
+'''
+    user_id = models.OneToOneField(User)
+    hood_id = models.ForeignKey(Neighborhood)
+
+    def __str__(self):
+        return self.user_id
+
+
+class Post(models.Model):
+    name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='images/', blank=True)
+    description = HTMLField(blank=True)
+    poster = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    post_hood = models.ForeignKey(
+        Neighborhood, on_delete=models.CASCADE, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    '''
+    this is added to ensure the linter has no errors saying class has no objects member in VS Code IDE
+    '''
+    objects = models.Manager()
+
+    @classmethod
+    def search_post(cls, search_term):
+        posts = cls.objects.filter(name__icontains=search_term)
+        return posts
+
+    @classmethod
+    def get_hood_posts(cls, post_hood):
+        posts = Post.objects.filter(post_hood=id)
+        return posts
+
+    @classmethod
+    def search_by_name(cls, search_term):
+        posts = cls.objects.filter(name__icontains=search_term)
+        return posts
+
+    @classmethod
+    def all_posts(cls,id):
+        posts = Post.objects.all()
+        return posts
