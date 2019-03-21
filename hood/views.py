f88b2831c4776bbbf69043987f0d356be22d316b
nziokaivy <nziokaivy@gmail.com>
2019-03-21 15:31:43 +0300

    add initial view and templates

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404,HttpResponseRedirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .models import Image, Profile, Comments,User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import  CommentForm, ImageForm
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
 
 # Create your views here.
+
+def home(request):
+    
+    return render(request, 'index.html')
